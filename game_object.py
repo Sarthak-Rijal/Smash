c76a9991473bf266e447a9d6f7b87b4e71792f24
Your Name <art@network.ru>
2019-03-09 03:32:08 -0800

    platform with two players working

---
+++ game_object.py
@@ -0,0 +1,34 @@
+import pygame
+from util import normalize
+
+WHITE = (255, 255, 255)
+
+class GameObject(pygame.sprite.Sprite):
+    # controls is a dict containing the keys left, right with their respective keys
+    def __init__(self, position, dimensions, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.width = normalize(dimensions[0], size[0])
+        self.height = normalize(dimensions[1], size[1])
+        self.size = size
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = normalize(position[0], size[0])
+        self.rect.y = normalize(position[1], size[1])
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+
+class Player(GameObject):
+    def __init__(self, position, dimensions, size, controls):
+        GameObject.__init__(self, position, dimensions, size)
+        self.controls = controls
+
+    def update(self):
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[self.controls['left']]:
+            self.move(-20, 0)
+        if pressed_key[self.controls['right']]:
+            self.move(20, 0)
